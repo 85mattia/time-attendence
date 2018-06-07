786503f2b7feef1dd04179ab34e5bcf2d6096f45
mattiaSt4 <mattiadevidi@gmail.com>
2018-06-07 17:44:11 +0200

    Add files via upload

---
+++ timeatt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import HomeWindowController
+
+
+
+
+
+if __name__ == '__main__':
+   
+    print("AVVIO APP")
+    
+    HomeWindowController.startApp()
+    print("Fine APP")
+    
+
+    
+
